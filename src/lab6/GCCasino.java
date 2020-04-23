1b869abc4788a423234bb0f6b9da30d35d2a678c
ashley <ashleykubera@gmail.com>
2020-04-23 16:49:22 -0400

    initial commit

---
+++ src/lab6/GCCasino.java
@@ -0,0 +1,77 @@
+package lab6;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GCCasino {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+
+		boolean decision = true;
+		
+		System.out.println("Welcome to the Grand Circus Casino!");
+		
+		System.out.println();
+		
+		System.out.println("How many sides should each die have?");
+		int diceSides = scnr.nextInt();
+		
+		while (decision) {
+			
+			System.out.println("Would you like to try your luck and roll those dice?");
+			System.out.print("Enter 1 for the casino floor or 0 if you're under 21 (US/ non-reservation). ");
+			
+			int chance = scnr.nextInt();
+			
+			switch (chance) {
+			
+			case 1: 
+				System.out.println("Roll 1:");
+				int roll1 = generateRandomDieRoll(diceSides);
+				int roll2 = generateRandomDieRoll(diceSides);
+				System.out.println(roll1);
+				System.out.println(roll2);
+				
+				funTimeRolls(roll1, roll2);
+				
+				break;
+			case 0:
+				System.out.println("Sorry you can't join us. Maybe in a few years!");
+				return;
+			default:
+				System.out.println("Invalid choice. Enter 1 or 0.");
+				break;
+			}
+			
+			System.out.println();
+			
+			System.out.println("Would you like to try your luck again? (y/n):");
+			
+			String answer = scnr.next();
+		
+			if (answer.equals("n")) {
+				decision = false;
+				System.out.println("Thanks for playing! Better luck next time!");
+				}
+		}
+	}
+	
+	private static int generateRandomDieRoll(int diceSides) {
+		Random randGen = new Random();
+		int roll = randGen.nextInt(diceSides) + 1;
+		return roll;
+	}
+	
+	private static void funTimeRolls(int roll1, int roll2) {
+
+		if (roll1 == roll2) {
+			System.out.println("You rolled snake eyes!");
+		} else if (roll1 == 6 && roll2 ==6) {
+			System.out.println("You rolled box cars!");
+		} else if (roll1 + roll2 == 7) {
+			System.out.println("You rolled craps!");
+		}
+	}
+	
+}
